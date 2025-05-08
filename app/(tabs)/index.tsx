04fe7fb0d7d57018109e5b235f63073a4540aa6f
NipunKohli12 <nipunkohli73073@gmail.com>
2025-05-08 17:09:03 +1000

    fixed typo in firebase config

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,7 @@ import '../../firebase.js';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-const {firebaseConfig} = require('../..firebase.js');
+const { firebaseConfig } = require('../../firebase.js');
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
